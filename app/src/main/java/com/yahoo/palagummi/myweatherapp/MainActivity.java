b6368dcfe9e3c03630851530b46e0d67990d7c3b
Durga Sai Preetham <durgasaipreethamp@yahoo.com>
2016-12-25 20:57:23 -0800

    Initial commit. Done with UI

---
+++ app/src/main/java/com/yahoo/palagummi/myweatherapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.yahoo.palagummi.myweatherapp;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
