7b9515edcc2efb67ba7ba0ac3671da4fbb93eb6b
Durga Sai Preetham <durgasaipreethamp@yahoo.com>
2016-12-25 20:58:21 -0800

    Done interacion with internet and get the weather info from weatherAPI

--- app/src/main/java/com/yahoo/palagummi/myweatherapp/MainActivity.java
+++ app/src/main/java/com/yahoo/palagummi/myweatherapp/MainActivity.java
@@ -16,10 +16,64 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText cityName;
+
+
+    public void findWeather(View view) {
+
+    }
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection;
+            try {
+                url = new URL(strings[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+                while(data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+                return result;
+            } catch(Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            JSONObject jsonObject;
+            JSONArray arr;
+            try {
+                jsonObject = new JSONObject(result);
+                String weatherInfo = jsonObject.getString("weather");
+                arr = new JSONArray(weatherInfo);
+                for(int i=0; i<arr.length(); i++) {
+                    JSONObject jsonPart = arr.getJSONObject(i);
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // get the cityName editText
+        cityName = (EditText) findViewById(R.id.cityName);
     }
 }
